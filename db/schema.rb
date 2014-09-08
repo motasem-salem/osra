3f450b049c5e6a876ddf37364b76cc1463a6b645
Sampriti Panda <sampdingo@hotmail.com>
2014-09-08 12:22:17 +0530

    Add sequential_id to Orphan List

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20140912082520) do
-=======
-ActiveRecord::Schema.define(version: 20140907055152) do
->>>>>>> Basic Model and Spec for OrphanList
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +75,7 @@ ActiveRecord::Schema.define(version: 20140907055152) do
     t.binary   "file"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "sequential_id"
   end
 
   create_table "organizations", force: true do |t|
